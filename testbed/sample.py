058588687e3b180d6d0ca4d98e5817edc106b554
dante825 <kangwei9125@gmail.com>
2019-09-29 22:33:01 +0800

    example from https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html

--- testbed/sample.py
+++ testbed/sample.py
@@ -1,30 +1,42 @@
 # https://medium.com/velotio-perspectives/real-time-text-classification-using-kafka-and-scikit-learn-c2875ad80b3c
+# https://scikit-learn.org/stable/tutorial/text_analytics/working_with_text_data.html
 
-from __future__ import division,print_function, absolute_import
-from sklearn.datasets import fetch_20newsgroups #built-in dataset
+from sklearn.datasets import fetch_20newsgroups
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.naive_bayes import MultinomialNB
 import pickle
 
 #Defining model and training it
-categories = ["talk.politics.misc","misc.forsale","rec.motorcycles",\
-"comp.sys.mac.hardware","sci.med","talk.religion.misc"] #http://qwone.com/~jason/20Newsgroups/ for reference
-def fetch_train_dataset(categories):
-    twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
-    return twenty_train
-def bag_of_words(categories):
-    count_vect = CountVectorizer()
-    X_train_counts = count_vect.fit_transform(fetch_train_dataset(categories).data)
-    pickle.dump(count_vect.vocabulary_, open("vocab.pickle", 'wb'))
-    return X_train_counts
-def tf_idf(categories):
-    tf_transformer = TfidfTransformer()
-    return (tf_transformer,tf_transformer.fit_transform(bag_of_words(categories)))
-def model(categories):
-    clf = MultinomialNB().fit(tf_idf(categories)[1], fetch_train_dataset(categories).target)
-    return clf
-model = model(categories)
-pickle.dump(model,open("model.pickle", 'wb'))
-print("Training Finished!")
-#Training Finished Here
+categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+#http://qwone.com/~jason/20Newsgroups/ for reference
+
+twenty_train = fetch_20newsgroups(subset='train', categories=categories, shuffle=True, random_state=42)
+print(twenty_train.target_names)
+
+count_vect = CountVectorizer()
+X_train_counts = count_vect.fit_transform(twenty_train.data)
+print('X_train_counts shape: ')
+print(X_train_counts.shape)
+
+count_vect.vocabulary_.get(u'algorithm')
+
+tf_transformer = TfidfTransformer(use_idf=False).fit(X_train_counts)
+X_train_tf = tf_transformer.transform(X_train_counts)
+print('X_train_tf shape')
+print(X_train_tf.shape)
+
+tfidf_transformer = TfidfTransformer()
+X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
+print('X_train_tfidf shape')
+print(X_train_tfidf.shape)
+
+clf = MultinomialNB().fit(X_train_tfidf, twenty_train.target)
+
+docs_new = ['God is love', 'OpenGL on the GPU is fast']
+X_new_counts = count_vect.transform(docs_new)
+X_new_tfidf = tfidf_transformer.transform(X_new_counts)
+
+predicted = clf.predict(X_new_tfidf)
+for doc, category in zip(docs_new, predicted):
+    print('%r => %s' % (doc, twenty_train.target_names[category]))
