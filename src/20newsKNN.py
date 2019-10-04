2d1ae41ad4395b17d51f0a29d332a06eccebf0d3
dante825 <kangwei9125@outlook.com>
2019-10-04 17:39:03 +0800

    subset the df before training

--- src/20newsKNN.py
+++ src/20newsKNN.py
@@ -13,22 +13,23 @@ np.set_printoptions(linewidth=320)
 
 df = pd.read_csv('../output/20newsGroup18828.csv')
 df['category_id'] = df.category.factorize()[0]
+df_2 = df[['text', 'category_id']]
 
 # Some details about the data
 # print(df.groupby(['category', 'category_id']).count().sort_values('category_id'))
 
-train_df, test_df = train_test_split(df, test_size=0.2)
+train_df, test_df = train_test_split(df_2, test_size=0.2)
 # print(train_df.groupby(['category', 'category_id']).count().sort_values('category_id'))
 # print(test_df.groupby(['category', 'category_id']).count().sort_values('category_id'))
 
 tfidf = TfidfVectorizer()
 X_train_features = tfidf.fit_transform(train_df.text)
-X_test_tfidf = tfidf.transform(test_df.text)
+X_test_features = tfidf.transform(test_df.text)
 
 classifier = KNeighborsClassifier(n_neighbors=6)
 classifier.fit(X_train_features, train_df.category_id)
 
-predicted = classifier.predict(X_test_tfidf)
+predicted = classifier.predict(X_test_features)
 # print(predict.shape)
 # print(predict)
 # print(classifier.predict_proba([[0.9]]))
