d4cb7d2d1ba4a6730e50a1dbef6f3563b6504302
dante825 <kangwei9125@outlook.com>
2019-10-16 11:00:28 +0800

    change the tf-idf generate function

--- src/GenTfIdf.py
+++ src/GenTfIdf.py
@@ -1,8 +1,13 @@
 """
 Move the TF-IDF part out of every script so it won't repeat in every script
 """
-from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.decomposition import NMF, TruncatedSVD, LatentDirichletAllocation
+from sklearn import preprocessing
+from sklearn import pipeline
+from sklearn.base import BaseEstimator, TransformerMixin
+from nltk.corpus import stopwords
 import pandas as pd
 import numpy as np
 
@@ -12,7 +17,9 @@ np.set_printoptions(linewidth=320)
 
 def generate_tfidf():
     df = pd.read_csv('../output/20newsGroup18828.csv')
-    df['category_id'] = df.category.factorize()[0]
+    # df['category_id'] = df.category.factorize()[0]
+    label = preprocessing.LabelEncoder()
+    df['category_id'] = label.fit_transform(df.category)
 
     # Some details about the data
     # print(df.groupby(['category', 'category_id']).count().sort_values('category_id'))
@@ -22,14 +29,33 @@ def generate_tfidf():
     # print(test_df.groupby(['category', 'category_id']).count().sort_values('category_id'))
 
     tfidf = TfidfVectorizer(analyzer='word', stop_words='english')
-    x_train_features = tfidf.fit_transform(train_df.text)
-    x_test_features = tfidf.transform(test_df.text)
-    # train_df['features'] = x_train_features
-    # test_df['features'] = x_test_features
+    x_train = tfidf.fit_transform(train_df.text)
+    x_test = tfidf.transform(test_df.text)
 
-    return train_df, test_df, x_train_features, x_test_features
+    return x_train, train_df.category_id, x_test, test_df.category_id
 
 
 def tfidf_generator():
     # https: // www.kaggle.com / dex314 / tfidf - truncatedsvd - and -light - gbm
     df = pd.read_csv('../output/20newsGroup18828.csv')
+
+    count_vect = CountVectorizer(analyzer=u'char', ngram_range=(1,8), max_features=1000,
+                                 strip_accents='unicode', stop_words='english', token_pattern=r'\w+')
+
+    tfidf = TfidfVectorizer(ngram_range=(1,3), max_features=1000, strip_accents='unicode', lowercase=True,
+                            analyzer='word', token_pattern=r'\w+', use_idf=True, smooth_idf=True, sublinear_tf=True,
+                            stop_words='english',)
+
+    nmfNC = 50
+    nmf = NMF(n_components=nmfNC, random_state=42, alpha=.1, l1_ratio=.5)
+
+    ldaNT = 50
+    lda = LatentDirichletAllocation(n_topics=ldaNT, max_iter=10, learning_method='online', learning_offset=50.,
+                                    random_state=42)
+
+    textNC = 150
+    tsvdText = TruncatedSVD(n_components=textNC, n_iter=25, random_state=42)
+    tsvdCount = TruncatedSVD(n_components=textNC, n_iter=25, random_state=42)
+
+    count_df = count_vect.fit_transform(df.text)
+    x_df = tfidf.fit_transform(count_df)
