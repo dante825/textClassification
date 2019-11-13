9e665607cfd578ca331d105060d18fbdd326cbf2
dante825 <kangwei9125@gmail.com>
2019-11-13 22:34:25 +0800

    Change abit to amend the result in the report

--- src/20newsKNN.py
+++ src/20newsKNN.py
@@ -8,24 +8,26 @@ from sklearn.neighbors import KNeighborsClassifier
 import pandas as pd
 import numpy as np
 import time
+import math
 
 pd.set_option('display.width', 320)
 np.set_printoptions(linewidth=320)
 
 start_time = time.time()
-x_train, y_train, x_test, y_test = generate_tfidf()
+# x_train, y_train, x_test, y_test = generate_tfidf()
 # x_train, y_train, x_test, y_test = generate_tfidf_svd(4000)
 # x_train, y_train, x_test, y_test = generate_tfidf_reduced(50)
 # x_train, y_train, x_test, y_test = generate_tf()
-# x_train, y_train, x_test, y_test = generate_tf_svd(4000)
-# x_train, y_train, x_test, y_test = generate_tf_reduced(500)
+x_train, y_train, x_test, y_test = generate_tf_svd(4000)
+# x_train, y_train, x_test, y_test = generate_tf_reduced(1000)
 print(x_train.shape)
 # print(y_train.shape)
 # print(x_test.shape)
 # print(y_test.shape)
 
+k = int(math.sqrt(x_train.shape[1]))
 # Rule of thumb for k is sqrt(sample size)
-classifier = KNeighborsClassifier(n_neighbors=127)
+classifier = KNeighborsClassifier(n_neighbors=k)
 classifier.fit(x_train, y_train)
 
 predicted = classifier.predict(x_test)
